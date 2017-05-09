080e3ead36cb59714d28a9b5683d1bd60737b999
Kamilius <kamiklviv@gmail.com>
2017-05-09 08:48:49 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+import { routing } from './app.routing';
+
+import 'normalize.css';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {}
+
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+
+  hmrOnDestroy(store) {
+    const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
